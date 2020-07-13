68a36a4a5ac135b5e3309d856e86edc0242fdfe2
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-07-13 11:46:11 -0400

    card pulls user post data from db

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,61 +1,45 @@
 import React, { useState, useContext } from 'react';
-import { Card, CardDeck, Row, Col, Container } from 'react-bootstrap';
+import { Card, CardDeck, Row, Col, Container, CardColumns } from 'react-bootstrap';
 
-function cardRow() {
-//   // get username out of context object to display in nav
-//   const { username } = useContext(UserInfoContext);
+// import context for global state
+import UserInfoContext from '../utils/UserInfoContext';
 
+import * as API from '../utils/API';
+import AuthService from '../utils/auth';
 
+function AllPosts() {
+  // get whole userData state object from App.js
+  const userData = useContext(UserInfoContext);
 
-return(
-<>
-<Container fluid>
-<Row>
-{/* <CardDeck> */}
-<Col>
-<Card border="primary" bg="dark" text="light" style={{padding: "20px", margin: "20px"}}>
-<Card.Header>Username</Card.Header>
-  <Card.Img variant="top" src="https://images.fineartamerica.com/images-medium-large-5/downtown-richmond-in-black-and-white-gordon-cain.jpg" />
-  <Card.Body>
+  //   // get username out of context object to display in nav
+  //   const { username } = useContext(UserInfoContext);
 
-    <Card.Title>Title</Card.Title>
-    <Card.Text>
-      Description of Product
-    </Card.Text>
-  </Card.Body>
-</Card>
-</Col>
-<Col>
-<Card border="primary" bg="dark" text="light" style={{padding: "20px", margin: "20px"}}>
-<Card.Header>Username</Card.Header>
-  <Card.Img variant="top" src="https://images.fineartamerica.com/images-medium-large-5/downtown-richmond-in-black-and-white-gordon-cain.jpg" />
-  <Card.Body>
-    
-    <Card.Title>Title</Card.Title>
-    <Card.Text>
-      Description of Product
-    </Card.Text>
-  </Card.Body>
-</Card>
-</Col>
-<Col>
-<Card border="primary" bg="dark" text="light" style={{padding: "20px", margin: "20px"}}>
-<Card.Header>Username</Card.Header>
-  <Card.Img variant="top" src="https://images.fineartamerica.com/images-medium-large-5/downtown-richmond-in-black-and-white-gordon-cain.jpg" />
-  <Card.Body>
-   
-    <Card.Title>Title</Card.Title>
-    <Card.Text>
-      Description of Product
-    </Card.Text>
-  </Card.Body>
-</Card>
-</Col>
-{/* </CardDeck> */}
-</Row>
-</Container>
-</>
-);
+  return (
+    <>
+      <Container fluid>
+        {/* <Row>
+          <Col> */}
+        <CardColumns>
+          {userData.posts.map((post) => {
+            return (
+              <Card border="primary" bg="dark" text="light" style={{ padding: "20px", margin: "20px" }}>
+                <Card.Header>{userData.username}</Card.Header>
+                <Card.Img variant="top" src="https://images.fineartamerica.com/images-medium-large-5/downtown-richmond-in-black-and-white-gordon-cain.jpg" />
+                <Card.Body>
+                  <Card.Title>{post.title}</Card.Title>
+                  <Card.Text>
+                    {post.description}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+        {/* </Col>
+        </Row> */}
+      </Container>
+    </>
+  );
 }
 
-export default cardRow;
+export default AllPosts;
