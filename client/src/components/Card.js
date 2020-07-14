db02d291b6f022429d96a67b99425fe16c9dba0a
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-07-13 20:55:17 -0400

    added email link

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { Card, CardDeck, Row, Col, Container, CardColumns } from 'react-bootstrap';
+import { Card, CardDeck, Row, Col, Container, CardColumns, Button } from 'react-bootstrap';
 
 // import context for global state
 import UserInfoContext from '../utils/UserInfoContext';
@@ -23,12 +23,19 @@ function AllPosts() {
           {userData.posts.map((post) => {
             return (
               <Card border="primary" bg="dark" text="light" style={{ padding: "20px", margin: "20px" }}>
-                <Card.Header>{userData.username}</Card.Header>
+                <Card.Header>{userData.username}
+                <Button href={`mailto: ${userData.email}`} variant="dark" style={{ fontSize: "45px"}}>
+                    &#9993;
+                  </Button>
+                </Card.Header>
                 <Card.Img variant="top" src="https://images.fineartamerica.com/images-medium-large-5/downtown-richmond-in-black-and-white-gordon-cain.jpg" />
                 <Card.Body>
                   <Card.Title>{post.title}</Card.Title>
                   <Card.Text>Description: {post.description}</Card.Text>
                   <Card.Text>Price: ${post.price}</Card.Text>
+                  {/* <Button href={`mailto: ${userData.email}`} variant="dark" style={{ fontSize: "45px"}}>
+                    &#9993;
+                  </Button> */}
                 </Card.Body>
               </Card>
             );
