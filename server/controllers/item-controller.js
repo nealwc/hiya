d7c720ef62417468b642ed5f6ab1e9a1f49e4f6a
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-07-14 21:30:07 -0400

    item table

--- server/controllers/item-controller.js
+++ server/controllers/item-controller.js
@@ -1,1 +1,14 @@
 const { Item } = require('../models');
+
+module.exports = {
+    // get all items
+    async getAllItems(req, res) {
+        const items = await Item.find();
+        return res.json(items);
+      },
+    // create an item
+    async createItem({ body }, res) {
+        const item = await Item.create(body);
+        res.json(item);
+    }
+}
