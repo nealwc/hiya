84f0e41edb2e9e70099af3d11d4e12b937bcb4c6
Jacob Patton <jspatton40@gmail.com>
2020-07-24 17:07:43 -0400

    switched tabs to link buttons

--- client/src/components/Tabs.js
+++ client/src/components/Tabs.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tabs, Tab, Image, Row, Col} from 'react-bootstrap';
+import { Navbar, Nav, Container, Modal, Tabs, Tab, Image, Row, Col, ButtonGroup, Button} from 'react-bootstrap';
 import SignUpForm from './SignupForm';
 import LoginForm from './LoginForm';
 // import Card from './Card';
@@ -22,32 +22,44 @@ function Tabs1() {
         <Container>
             <Row className='categories'>
                 <Col>
-                    <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" style={{ marginBottom: '45px'}}>
-                        <Tab
-                            eventKey="all"
-                            title="All">
-                        </Tab>
-                        <Tab
-                            eventKey="computers"
-                            title="Computers">
-                        </Tab>
-                         <Tab
-                            eventKey="tablets/phones"
-                            title="Tablets/Phones">
-                        </Tab>
-                        <Tab
-                            eventKey="parts"
-                            title="Parts">
-                        </Tab>
-                        <Tab
-                            eventKey="services"
-                            title="Services">
-                        </Tab>
-                        <Tab 
-                            eventKey="other"
-                            title="Other">
-                        </Tab>
-                </Tabs>
+                    <ButtonGroup defaultActiveKey="profile" id="categories" size='lg' variant='link' style={{ marginBottom: '45px'}}>
+                        <Button
+                            // eventKey="all"
+                            // title="All"
+                            variant='link'
+                            >All
+                        </Button>
+                        <Button
+                            // eventKey="computers"
+                            // title="Computers"
+                            variant='link'
+                            >Computers
+                        </Button>
+                         <Button
+                            // eventKey="tablets/phones"
+                            // title="Tablets/Phones"
+                            variant='link'
+                            >Tablets/Phones
+                        </Button>
+                        <Button
+                            // eventKey="parts"
+                            // title="Parts"
+                            variant='link'
+                            >Parts
+                        </Button>
+                        <Button
+                            // eventKey="services"
+                            // title="Services"
+                            variant='link'
+                            >Services
+                        </Button>
+                        <Button 
+                            // eventKey="other"
+                            // title="Other"
+                            vat riant='link'
+                            >Other
+                        </Button>
+                </ButtonGroup>
             </Col>
         </Row>
     </Container>
