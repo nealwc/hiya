790d77dc58452c1ff931a15e7295afe5c048e123
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-07-28 23:34:10 -0400

    photos upload

--- client/src/pages/PostForm.js
+++ client/src/pages/PostForm.js
@@ -5,9 +5,13 @@ import { useHistory } from 'react-router-dom';
 import AuthService from '../utils/auth';
 import { saveItem } from '../utils/API';
 
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/doj6qnxvv/image/upload";
+const CLOUDINARY_UPLOAD_PRESET = "fb30ycv1";
+
 function BlankForm() {
     const history = useHistory();
     const [itemFormData, setItemFormData] = useState({
+        photo: '',
         title: '',
         description: '',
         price: '',
@@ -17,6 +21,26 @@ function BlankForm() {
     function handleInputChange(e) {
         const { name, value } = e.target;
         setItemFormData({ ...itemFormData, [name]: value })
+    };
+
+    const handleUploadImage = async e => {
+        const files = e.target.files
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+        const res = await fetch(CLOUDINARY_URL,
+            {
+              method: 'POST',
+              body: data
+            }
+          );
+      
+          const file = await res.json();
+      
+          console.log(file);
+      
+          setItemFormData({ photo : file.secure_url });
     };
 
     // function to handle saving an item to our database
@@ -44,7 +68,13 @@ function BlankForm() {
                     <Card.Body>
                         <Form onSubmit={handleSaveItem} bg="dark" variant="dark">
                             <Form.Group>
-                                <Form.File id="exampleFormControlFile1" label="Add a photo!" />
+                                <Form.File 
+                                id="itemImage" 
+                                label="Add a photo!"
+                                name="image"
+                                type="file"
+                                onChange={handleUploadImage}
+                                />
                             </Form.Group>
                             <Form.Group controlId="formBasicTitle">
                                 <Form.Label>What would you like to offer? *</Form.Label>
