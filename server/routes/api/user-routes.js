892023adcddeb2fa82755a6ff84760b37773e31e
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-11-15 21:06:21 -0500

    commented unused code

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -3,8 +3,8 @@ const {
   createUser,
   getAllUsers,
   getSingleUser,
-  saveBook,
-  deleteBook,
+  // saveBook,
+  // deleteBook,
   login,
   saveItem
 } = require('../../controllers/user-controller');
@@ -24,6 +24,6 @@ router.route('/me').get(authMiddleware, getSingleUser);
 
 router.route('/:username').get(getSingleUser);
 
-router.route('/books/:id').delete(authMiddleware, deleteBook);
+// router.route('/books/:id').delete(authMiddleware, deleteBook);
 
 module.exports = router;
